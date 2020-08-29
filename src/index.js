bb643f27fadae233f7cd96e7e11d892dbadb5ac2
Jelani Woods <jelani@firstdraft.com>
2020-08-28 22:14:36 -0500

    Try drag n dropping
    It's just that easy babey

--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Basic from './Basic';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <Basic />
   </React.StrictMode>,
   document.getElementById('root')
 );
